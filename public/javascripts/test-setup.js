0b03927a72362219c8e5b85ee7a582378061a303
Christopher Weiss <info@cweiss.de>
2013-02-08 09:49:36 +0100

    cleanup on test-setup

--- public/javascripts/test-setup.js
+++ public/javascripts/test-setup.js
@@ -1,16 +1,12 @@
 //Models for testing out Backbone Factory
 
 var User = Backbone.Model.extend({
-
   name: null,
   email: null
-
 });
 
 var Post = Backbone.Model.extend({
-
   defaults: {
     title: 'Default Title'
   }
-
 });
